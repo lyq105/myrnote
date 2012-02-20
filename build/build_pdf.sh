6a9b2ee714e9f0a8a45d40d7a84350ee7cf3defd
liyiqiang <liyiqiang@mail.nwpu.edu.cn>
2012-02-20 16:23:32 +0800

    Change Latex compiler from latex + dvipdfmx to xelatex

--- build/build_pdf.sh
+++ build/build_pdf.sh
@@ -3,15 +3,15 @@
 
 
 
-latex main_file
+xelatex main_file
 
 for bufile in `ls bu*.aux`
 	do
 		bibtex $bufile
 	done
 
-latex main_file
-latex main_file
-dvipdfmx main_file
+xelatex main_file
+xelatex main_file
+#dvipdfmx main_file
 
 #rm *.aux *.dvi *.out *.toc *.blg *.bbl
